033e7c07cb83c1bccfba176206fbd8889c855bd8
pongpatapee <pongpatapee1021@gmail.com>
2022-07-25 14:41:50 -0700

    fixed bugs and attatched controls to neural network

--- car.js
+++ car.js
@@ -12,8 +12,12 @@ class Car {
         this.angle = 0;
         this.damage = false;        
 
+        this.useBrain = controleType == "AI";
+
         if(controleType != "DUMMY") {
             this.sensor = new Sensor(this);
+            //first layer's input is the number of ray count, 6 is arbitrary, 4 is the number of controls
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
         }
         this.controls = new Controls(controleType);
     }
@@ -44,6 +48,16 @@ class Car {
         }
         if(this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(s => s == null ? 0 : 1 - s.offset)
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            console.log(outputs)
+
+            if(this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
--- network.js
+++ network.js
@@ -1,15 +1,15 @@
 class NeuralNetwork {
   constructor(neuronCounts) {
     this.levels = [];
-    for(let i = 0; i < neuronCounts - 1; i++) {
+    for(let i = 0; i < neuronCounts.length - 1; i++) {
       this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
     }
   }
 
   static feedForward(givenInputs, network) {
     let outputs = Level.feedForward(givenInputs, network.levels[0]);
-    for(let i = 1; i < network.length; i++) {
-      Level.feedForward(outputs, network.levels[i]);
+    for(let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.levels[i]);
     }
     return outputs
   }
@@ -34,13 +34,13 @@ class Level {
   }
 
   static #randomize(level) {
-    for(let i = 0; i < this.inputs.length; i++) {
-      for(let j = 0; j < this.outputs.length; j++) {
+    for(let i = 0; i < level.inputs.length; i++) {
+      for(let j = 0; j < level.outputs.length; j++) {
         level.weights[i][j] = Math.random() * 2 - 1; // initialize weights with random number between -1 and 1
       }
     }
 
-    for(let i = 0; i < this.biases.length; i++) {
+    for(let i = 0; i < level.biases.length; i++) {
       level.biases[i] = Math.random() * 2 - 1; // same initialization with biases 
     }
   }
@@ -53,18 +53,18 @@ class Level {
     for(let i = 0; i < level.outputs.length; i++) {
       let sum = 0;
       for(let j = 0; j < level.inputs.length; j++) {
-        sum += inputs[j] * weights[j][i];
+        sum += level.inputs[j] * level.weights[j][i];
       }
 
       if(sum > level.biases[i]) {
         level.outputs[i] = 1;
       } else {
-        level.outputCount[i] = 0;
+        level.outputs[i] = 0;
       }
 
     }
 
-    return levels.output;
+    return level.outputs;
   }
 
 }
